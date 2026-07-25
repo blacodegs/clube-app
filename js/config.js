4fff6e58b63298bb6a2720274f7d7d95abdf8294
Edicarlos <edicarlos.mail@gmail.com>
2026-07-25 15:13:22 -0300

    Novo google client id e novo endpoint

--- js/config.js
+++ js/config.js
@@ -3,6 +3,5 @@
  * Preencha estas duas constantes após publicar o Apps Script como Web App
  * e criar o Client ID do Google.
  */
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx9KWySVUePMxnkGB47cX7GPRHRriz3is9HyCYx2JSeOatifOlRGu7w63jdcapoPDhRQw/exec';
-//const GOOGLE_CLIENT_ID = '458679085864-k7gtcm027liif1rr16tporp0kfbr34gi.apps.googleusercontent.com';
-const GOOGLE_CLIENT_ID = '458679085864-rco4r41cl4plmgnfelec48t0e9jde3r8.apps.googleusercontent.com';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzfks9bPEqNzkeTTLQawR92U80P3FTdgMtfEp65qUlByQMh5rVkp4-3_Ee10k5qTltL/exec';
+const GOOGLE_CLIENT_ID = '480101887690-sh8ik0ts05pfq1uhpv6r33d6j7gut7mk.apps.googleusercontent.com';
