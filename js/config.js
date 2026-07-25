0eceb043c63a518464da5577285bea3c0bc057c5
Edicarlos <edicarlos.mail@gmail.com>
2026-07-25 12:38:17 -0300

    Novo GOOGLE_CLIENT_ID e novo APPS_SCRIPT_URL

--- js/config.js
+++ js/config.js
@@ -3,5 +3,6 @@
  * Preencha estas duas constantes após publicar o Apps Script como Web App
  * e criar o Client ID do Google.
  */
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx0aOQPGglfgtpA65MgT5EYSS5aRPRyBQASqyq2rLS7FKPaY7h3JRPgb9Gtw8CpDJ7Ytg/exec';
-const GOOGLE_CLIENT_ID = '458679085864-k7gtcm027liif1rr16tporp0kfbr34gi.apps.googleusercontent.com';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx9KWySVUePMxnkGB47cX7GPRHRriz3is9HyCYx2JSeOatifOlRGu7w63jdcapoPDhRQw/exec';
+//const GOOGLE_CLIENT_ID = '458679085864-k7gtcm027liif1rr16tporp0kfbr34gi.apps.googleusercontent.com';
+const GOOGLE_CLIENT_ID = '458679085864-rco4r41cl4plmgnfelec48t0e9jde3r8.apps.googleusercontent.com';
